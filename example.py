4eb61f5d46bf9d925f3a74f61ba36e5d5a961ad8
vitorramos <ramos.vitor89@gmail.com>
2019-11-19 10:58:57 +0100

    2d plot example

--- example.py
+++ example.py
@@ -1,4 +1,16 @@
 from pascaldata import PascalData
+from matplotlib import pyplot as plt
 
+# parsing the json file
 data= PascalData("data/completo_black_3.json")
-print(data.times())
+# creating the dataframe
+df= data.times()
+# converting string to float
+df= df.astype(float)
+# checking the available variables
+print(df.columns)
+# filtering the data
+df= df[ (df["cores"] == 16)&(df["input"] == 5) ]
+# ploting
+df.plot(x="frequency", y="total_time")
+plt.show()
